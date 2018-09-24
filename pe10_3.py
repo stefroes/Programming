4eaa4a08807cac6375fc229986780f4720125d01
Stef Roes <info@roeswebdesign.nl>
2018-09-24 21:38:55 +0200

    PE 10.3

---
+++ pe10_3.py
@@ -0,0 +1,20 @@
+import xmltodict
+
+with open('Stations.xml') as file:
+    stations = xmltodict.parse(file.read())['Stations']['Station']
+
+    print('Dit zijn de codes en types van de 4 stations:')
+    for station in stations:
+        print(station['Code'].ljust(4) + ' - ' + station['Type'])
+
+    print('\nDit zijn alle stations met één of meer synoniemen:')
+    for station in stations:
+            if station['Synoniemen']:
+                print(station['Synoniemen'])
+
+    print('\nDit is de lange naam van elk station:')
+    for station in stations:
+        names = []
+        for name in station['Namen']:
+            names.append(station['Namen'][name])
+        print(station['Code'].ljust(4) + ' - ' + max(names, key=len))
